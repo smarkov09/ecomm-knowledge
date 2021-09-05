cecfd87122f460efee283a35cf397668ddf18fe8
smarkov09 <rmnbilc09@gmail.com>
2021-09-05 12:33:50 -0500

    completed sql structure

--- db-structure/db-structure.sql
+++ db-structure/db-structure.sql
@@ -18,3 +18,45 @@ CREATE TABLE 'orders' (
 	KEY 'user_id' ('user_id'),
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE 'pages' (
+	'id' MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	'category_id' SMALLINT UNSIGNED NOT NULL,
+	'title' VARCHAR(100) NOT NULL,
+	'description' TINYTEXT NOT NULL,
+	'content' LONGTEXT NOT NULL,
+	'date_created' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY ('id'),
+	KEY 'category_id' ('category_id'),
+	KEY 'creation_date' ('date_created')
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE 'pdfs' (
+	'id' SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	'tmp_name' CHAR(40) NOT NULL,
+	'title' VARCHAR(100) NOT NULL,
+	'description' TINYTEXT NOT NULL,
+	'file_name' VARCHAR(40) NOT NULL,
+	'size' MEDIUMINT UNSIGNED NOT NULL,
+	'date_created' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY ('id'),
+	UNIQUE KEY 'tmp_name' ('tmp_name'),
+	KEY 'date_created' ('date_created')
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE 'users' (
+	'id' INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	'type' ENUM('member', 'admin') NOT NULL,
+	'username' VARCHAR(30) NOT NULL,
+	'email' VARCHAR(80) NOT NULL,
+	'pass' VARBINARY(32) NOT NULL,
+	'first_name' VARCHAR(20) NOT NULL,
+	'last_name' VARCHAR(40) NOT NULL,
+	'date_expires' DATE NOT NULL,
+	'date_created' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	'date_modified' TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+	PRIMARY KEY ('id'),
+	UNIQUE KEY 'username' ('username'),
+	UNIQUE KEY 'email' ('email')
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
